b0b268184b8eeb888ca2fc69a3b2abc97e4c5110
Kayode Olayiwola <caurdez02@gmail.com>
2023-02-17 11:28:09 -0800

    updates

--- js/main.js
+++ js/main.js
@@ -52,13 +52,6 @@
         }
     });
 
-
-    // Facts counter
-    $('[data-toggle="counter-up"]').counterUp({
-        delay: 10,
-        time: 2000
-    });
-    
     
     // Back to top button
     $(window).scroll(function () {
@@ -125,56 +118,6 @@
         }
     });
 
-
-
-    // Testimonials carousel
-    $(".testimonial-carousel").owlCarousel({
-        autoplay: true,
-        smartSpeed: 1500,
-        dots: true,
-        loop: true,
-        center: true,
-        responsive: {
-            0:{
-                items:1
-            },
-            576:{
-                items:1
-            },
-            768:{
-                items:2
-            },
-            992:{
-                items:3
-            }
-        }
-    });
-
-
-    // Vendor carousel
-    $('.vendor-carousel').owlCarousel({
-        loop: true,
-        margin: 45,
-        dots: false,
-        loop: true,
-        autoplay: true,
-        smartSpeed: 1000,
-        responsive: {
-            0:{
-                items:2
-            },
-            576:{
-                items:4
-            },
-            768:{
-                items:6
-            },
-            992:{
-                items:8
-            }
-        }
-    });
-
      // Porfolio isotope and filter
   $(window).on('load', function() {
     var portfolioIsotope = $('.portfolio-container').isotope({
@@ -200,12 +143,12 @@
   });
 
    // Portfolio details carousel
-   $(".portfolio-details-carousel").owlCarousel({
-    autoplay: true,
-    dots: true,
-    loop: true,
-    items: 1
-  });
+//    $(".portfolio-details-carousel").owlCarousel({
+//     autoplay: true,
+//     dots: true,
+//     loop: true,
+//     items: 1
+//   });
 
    // Init AOS
    function aos_init() {
